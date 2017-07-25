38d871b4a3b2a16f616bb5568f0d206f072d7ce5
Rayzian <zhouxiaoxi0516@sohu.com>
2017-07-25 16:39:09 +0800

    修改代码

--- spiderProject/HotelInfo/hotelSeleniumSpider.py
+++ spiderProject/HotelInfo/hotelSeleniumSpider.py
@@ -1,19 +1,15 @@
-#coding:utf-8
-# import datetime
+# coding:utf-8
+
 import codecs
 import datetime
 from bs4 import BeautifulSoup
-
 from selenium import webdriver
-import selenium.webdriver.support.ui as ui
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 import time
 
 
 class QunaSpider(object):
-
     def get_hotel(self, driver, to_city, fromdate, todate):
 
         ele_toCity = driver.find_element_by_name('toCity')
@@ -34,7 +30,7 @@ class QunaSpider(object):
                 WebDriverWait(driver, 10).until(
                     EC.title_contains(unicode(to_city))
                 )
-            except Exception,e:
+            except Exception, e:
                 print e
                 break
             time.sleep(5)
@@ -48,7 +44,7 @@ class QunaSpider(object):
             infos = soup.find_all(class_="item_hotel_info")
             f = codecs.open(unicode(to_city) + unicode(fromdate) + u'.txt', 'a', 'utf-8')
             for info in infos:
-                f.write(str(page_num)+'--' * 45)
+                f.write(str(page_num) + '--' * 45)
                 content = info.get_text().replace(" ", "").replace("\t", "").strip()
                 for line in [ln for ln in content.splitlines() if ln.strip()]:
                     f.write(line)
@@ -56,28 +52,27 @@ class QunaSpider(object):
             f.close()
             try:
                 next_page = WebDriverWait(driver, 10).until(
-                EC.visibility_of(driver.find_element_by_css_selector(".item.next"))
+                    EC.visibility_of(driver.find_element_by_css_selector(".item.next"))
                 )
                 next_page.click()
                 page_num += 1
                 time.sleep(10)
-            except Exception,e:
+            except Exception, e:
                 print e
                 break
 
-    def crawl(self,root_url,to_city):
-         today = datetime.date.today().strftime('%Y-%m-%d')
-         tomorrow=datetime.date.today() + datetime.timedelta(days=1)
-         tomorrow = tomorrow.strftime('%Y-%m-%d')
-         driver = webdriver.Firefox(executable_path=r'D:\fireFoxDrive\geckodriver.exe')
-         driver.set_page_load_timeout(50)
-         driver.get(root_url)
-         # driver.maximize_window()
-         driver.implicitly_wait(10)
-         self.get_hotel(driver, to_city, today, tomorrow)
+    def crawl(self, root_url, to_city):
+        today = datetime.date.today().strftime('%Y-%m-%d')
+        tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+        tomorrow = tomorrow.strftime('%Y-%m-%d')
+        driver = webdriver.Firefox(executable_path=r'D:\fireFoxDrive\geckodriver.exe')
+        driver.set_page_load_timeout(50)
+        driver.get(root_url)
+        # driver.maximize_window()
+        driver.implicitly_wait(10)
+        self.get_hotel(driver, to_city, today, tomorrow)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     spider = QunaSpider()
     spider.crawl('http://hotel.qunar.com/', u"成都")
-
